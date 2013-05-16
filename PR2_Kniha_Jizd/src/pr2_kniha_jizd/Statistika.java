cae9a3cb0bfc034429ca38b022f79045372f52bf
delta <delta@wced-136-218-32-147.feld.cvut.cz>
2013-05-16 02:56:13 +0200

    Rozdělena na statistiku aut a statistiku řidičů

--- PR2_Kniha_Jizd/src/pr2_kniha_jizd/Statistika.java
+++ PR2_Kniha_Jizd/src/pr2_kniha_jizd/Statistika.java
@@ -1,21 +1,50 @@
 package pr2_kniha_jizd;
-import javax.swing.JDialog;  
-public class Statistika extends JDialog{
-    private TabelPanel tabulka;
-    public Statistika() {
-        print();
-    }
-    private void print(){
-        
-       tabulka = new TabelPanel("CAR","DRIVER"); // vytvoření tabulky vstupní jako vstup sou názvy 2 tabulek
-        this.add(tabulka);    
-        
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Label;
+import javax.swing.BoxLayout;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+public class Statistika extends JDialog {
+
+    private TabelPanel tabulkaCar;
+    private TabelPanel tabulkaDriver;
+
+    public Statistika(boolean Drivers) {
+        if (Drivers) {
+            printDrivers();
+        } else {
+            printCars();
+        }
         this.setResizable(false);
         this.setModal(true);
         this.pack();
         this.setSize(600, 600);
         this.setLocation(WIDTH, HEIGHT);
         setLocationRelativeTo(getRootPane());
-    
-    }    
+
+    }
+
+    private void print() {
+
+        tabulkaCar = new TabelPanel(); // vytvoření tabulky vstupní jako vstup sou názvy 2 tabulek
+        tabulkaCar.setToCar();
+        this.add(tabulkaCar);
+
+    }
+
+    private void printDrivers() {
+        tabulkaDriver = new TabelPanel(); // vytvoření tabulky vstupní jako vstup sou názvy 2 tabulek
+        tabulkaDriver.setToDriver();
+        this.add(tabulkaDriver);
+    }
+
+    private void printCars() {
+        JPanel tabulky = new JPanel(new FlowLayout());
+        tabulkaCar = new TabelPanel(); // vytvoření tabulky vstupní jako vstup sou názvy 2 tabulek
+        tabulkaCar.setToCar();
+        this.add(tabulkaCar);
+    }
 }
